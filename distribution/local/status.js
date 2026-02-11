e2e5d740469fd7b66da1a2ff9be245e626424032
Anirudh Kondapaneni <kondapaneni.ani@gmail.com>
2026-02-11 17:09:18 -0500

    T2 done, tested using REPL

--- distribution/local/status.js
+++ distribution/local/status.js
@@ -1,4 +1,5 @@
 // @ts-check
+const id = require("../util/id.js");
 /**
  * @typedef {import("../types.js").Callback} Callback
  * @typedef {import("../types.js").Node} Node
@@ -9,7 +10,48 @@
  * @param {Callback} callback
  */
 function get(configuration, callback) {
-  return callback(new Error('status.get not implemented'));
+  // return callback(new Error('status.get not implemented'));
+  const node = global.distribution.node;
+  const config = node.config;
+
+  if (!config || !config.ip || !config.port) {
+    return callback(new Error("Node config not initialized"));
+  }
+
+  if (global.distribution.counts === undefined) {
+    global.distribution.counts = 0;
+  }
+
+  const mem = process.memoryUsage();
+
+  let result;
+
+  if (configuration === "ip") {
+    result = config.ip;
+  }
+  else if (configuration === "port") {
+    result = config.port;
+  }
+  else if (configuration === "nid") {
+    result = id.getNID(config);
+  }
+  else if (configuration === "sid") {
+    result = id.getSID(config);
+  }
+  else if (configuration === "counts") {
+    result = global.distribution.counts;
+  }
+  else if (configuration === "heapTotal") {
+    result = mem.heapTotal;
+  }
+  else if (configuration === "heapUsed") {
+    result = mem.heapUsed;
+  }
+  else {
+    return callback(new Error("Unknown status property"));
+  }
+
+  callback(null, result);
 };
 
 
